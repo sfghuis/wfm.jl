fbec53b938b549f743bc2952beab51fc7b9408f3
sfghuis <sfgroothuis@Hotmail.com>
2023-11-19 12:14:07 +0100

    add erlang test setup

--- test/queue/test_erlang.jl
+++ test/queue/test_erlang.jl
@@ -1,12 +1,42 @@
-using Test
 
 include(joinpath(@__DIR__,"src", "queue", "erlang.jl"))
 
+using Test
 
 @testitem "Sanity test" begin
     @test 1 + 1 == 2
     @test_throws DomainError sqrt(-1)
 end
+
+@testitem "Erlang C intensity calculation" begin
+    
+    test_data = ErlangCData(arrivals=200, interval=60, handling_time=3, speed_of_answer=20/60, num_agents=10, service_level=.80)
+    @test traffic_intensity(test_data) == 10
+end
+
+# # Test probability_zero_customers function
+# @testset "Probability Zero Customers Tests" begin
+#     test_data = ErlangCData(arrivals=200, interval=60, handling_time=3, speed_of_answer=20/60, num_agents=10, service_level=.80)
+#     @test isapprox(ErlangC.probability_zero_customers(data), expected_result, atol=1e-5)
+# end
+
+# Test probability_queued function
+@testset "Probability Queued Tests" begin
+    test_data = ErlangCData(arrivals=100, interval=30, handling_time=3, speed_of_answer=20/60, num_agents=10, service_level=.80)
+    @test isapprox(ErlangC.probability_queued(test_data), 0.174, atol=1e-5)
+end
+
+# # Test average_customers function
+# @testset "Average Customers Tests" begin
+#     test_data = ErlangCData(arrivals=200, interval=60, handling_time=3, speed_of_answer=20/60, num_agents=10, service_level=.80)
+#     @test isapprox(ErlangC.average_customers(data), expected_result, atol=1e-5)
+# end
+
+# # Test agents_required function
+# @testset "Agents Required Tests" begin
+#     test_data = ErlangCData(arrivals=200, interval=60, handling_time=3, speed_of_answer=20/60, num_agents=10, service_level=.80)
+#     @test isapprox(ErlangC.agents_required(data), expected_result, atol=1e-5)
+# end
 
 @testitem "ErlangCData Tests" begin
 
@@ -16,26 +46,20 @@ end
     @test ErlangCData(arrivals=50.0, handling_time=0.1, speed_of_answer=0.05, num_agents=10, service_level=.8) isa ErlangCData
 
     #Invalid arrivals (negative value)
-    @test_throws ArgumentError ErlangCData(arrivals=-50.0, handling_time=0.1, speed_of_answer=0.05, num_agents=10, service_level=.8)
+    @test_throws ArgumentError  ErlangCData(arrivals=-50.0, handling_time=0.1, speed_of_answer=0.05, num_agents=10, service_level=.8)
 
     # Invalid interval (non-interger)
-    @test_throws ArgumentError ErlangCData(arrivals=50.0, interval=.8, handling_time=0.1, speed_of_answer=0.05, num_agents=10, service_level=1.2)
+    @test_throws ArgumentError  ErlangCData(arrivals=50.0, interval=.8, handling_time=0.1, speed_of_answer=0.05, num_agents=10, service_level=1.2)
 
     # Invalid handling_time (negative value)
-    @test_throws ArgumentError ErlangCData(arrivals=50.0, handling_time=-0.1, speed_of_answer=0.05, num_agents=10, service_level=.8)
+    @test_throws ArgumentError  ErlangCData(arrivals=50.0, handling_time=-0.1, speed_of_answer=0.05, num_agents=10, service_level=.8)
 
     # Invalid speed_of_answer (negative value)
-    @test_throws ArgumentError ErlangCData(arrivals=50.0, handling_time=0.1, speed_of_answer=-0.05, num_agents=10, service_level=.8)
+    @test_throws ArgumentError  ErlangCData(arrivals=50.0, handling_time=0.1, speed_of_answer=-0.05, num_agents=10, service_level=.8)
 
     # Invalid num_agents (negative value)
-    @test_throws ArgumentError ErlangCData(arrivals=50.0, handling_time=0.1, speed_of_answer=0.05, num_agents=-10, service_level=.8)
+    @test_throws ArgumentError  ErlangCData(arrivals=50.0, handling_time=0.1, speed_of_answer=0.05, num_agents=-10, service_level=.8)
 
     # Invalid service_level (greater than 100)
-    @test_throws ArgumentError ErlangCData(arrivals=50.0, handling_time=0.1, speed_of_answer=0.05, num_agents=10, service_level=1.2)
-end
-
-@testitem "Erlang C intensity calculation" begin
-    
-    test_data = ErlangCData(arrivals=200, interval=60, handling_time=3, speed_of_answer=20/60, num_agents=10, service_level=.80)
-    @test traffic_intensity(test_data) == 10
+    @test_throws ArgumentError  ErlangCData(arrivals=50.0, handling_time=0.1, speed_of_answer=0.05, num_agents=10, service_level=1.2)
 end
