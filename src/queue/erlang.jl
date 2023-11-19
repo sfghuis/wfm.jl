9cfb3eee157125ed2e0d2c1e97a2d26a7cddef27
sfghuis <sfgroothuis@Hotmail.com>
2023-11-19 10:53:25 +0100

    fix comments

--- src/queue/erlang.jl
+++ src/queue/erlang.jl
@@ -1,3 +1,5 @@
+module ErlangC
+
 using Parameters
 
 export ErlangCData, traffic_intensity
@@ -11,12 +13,26 @@ export ErlangCData, traffic_intensity
     service_level::Float64 = 0.8
 
     function ErlangCData(arrivals, interval, handling_time, speed_of_answer, num_agents, service_level)
+        """
+        Create an instance of ErlangCData.
+
+        Parameters:
+        - arrivals: Number of arrivals.
+        - interval: Time interval.
+        - handling_time: Call handling time.
+        - speed_of_answer: Service time.
+        - num_agents: Number of servers.
+        - service_level: Desired service level (between 0 and 1).
+
+        Returns:
+        An ErlangCData object.
+        """
         @assert arrivals >= 0 || throw(ArgumentError("Arrival rate must be a non-negative number."))
         @assert interval >= 0 || throw(ArgumentError("Interval rate must be a non-negative number."))
         @assert handling_time > 0.0 || throw(ArgumentError("Call duration must be a positive number."))
         @assert speed_of_answer >= 0.0 || throw(ArgumentError("Service time must be a positive number."))
         @assert num_agents >= 0 || throw(ArgumentError("Number of servers must be a positive integer."))
-        @assert 0.0 <= service_level <= 1.0 || throw(ArgumentError("Service level must be between 0 and 100."))
+        @assert 0.0 <= service_level <= 1.0 || throw(ArgumentError("Service level must be between 0 and 1."))
 
         return new(arrivals, interval, handling_time, speed_of_answer, num_agents, service_level)
     end
@@ -28,4 +44,6 @@ function traffic_intensity(data::ErlangCData)
     Calculate the arrival intensity
     """
     return (data.arrivals * data.handling_time) / data.interval
+end
+
 end
